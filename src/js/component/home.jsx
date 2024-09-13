1a4135890a434ce18f562d7ea1eb2e72136ffa34
Juan Carlos Díaz <169100845+Nxusbets@users.noreply.github.com>
2024-09-13 04:13:31 +0000

    primer cambio

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
+
 //create your first component
 const Home = () => {
+
+	//let nuevoTodo = "Tarea de ejemplo";
+	 const [nuevoTodo, setNuevoTodo] = useState("Tarea de ejemplo");
+
+const handleClick = () => {
+	console.log("Nueva tarea", nuevoTodo);
+}
+
+const handleChange = (evento) => {
+	console.log(event.target.value)
+	setNuevoTodo(event.target.value);
+
+}
+
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+				<div>
+					<input type = "text" onChange={handleChange}/>
+					<button onClick={handleClick}>
+						Agregar tarea
+					</button>
+				</div>
+				<p>Nueva tarea: {nuevoTodo}</p>
 		</div>
 	);
 };
