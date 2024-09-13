00c60bbe4dc01e7b7d312f8fee7adfb01f50c048
Juan Carlos Díaz <169100845+Nxusbets@users.noreply.github.com>
2024-09-13 04:41:46 +0000

    segundo cambio

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,20 +10,30 @@ const Home = () => {
 
 	//let nuevoTodo = "Tarea de ejemplo";
 	 const [nuevoTodo, setNuevoTodo] = useState("Tarea de ejemplo");
+	 const [todos, setTodos] = useState (["Una tarea de prueba", "Dos tareas de prueba"]);
 
 const handleClick = () => {
 	console.log("Nueva tarea", nuevoTodo);
+
+	setTodos([...todos, nuevoTodo])
+}
+
+const deleteTodo = (indice) => {
+
+	const listaNueva = todos.filter((todo, i) => i !== indice)
+	setTodos(listaNueva);
+
 }
 
 const handleChange = (evento) => {
-	console.log(event.target.value)
+	
 	setNuevoTodo(event.target.value);
 
 }
 
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
+			<h1 className="text-center mt-5">To do list Usando React + Fetch</h1>
 				<div>
 					<input type = "text" onChange={handleChange}/>
 					<button onClick={handleClick}>
@@ -31,6 +41,15 @@ const handleChange = (evento) => {
 					</button>
 				</div>
 				<p>Nueva tarea: {nuevoTodo}</p>
+				<ul>
+					{todos.map((todo, indice) => {
+						return (
+							<li>
+								{todo} <button onClick={() => deleteTodo(indice) } >Borrar</button>
+							</li>
+						)
+					})}
+				</ul>
 		</div>
 	);
 };
